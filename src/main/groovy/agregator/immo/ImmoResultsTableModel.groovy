e62106454bff07fa4365e95bc13f4ab079bd54e5
remi <remi@rvkb.com>
2010-08-25 15:15:01 +0200

    added price to immo GUI

--- src/main/groovy/agregator/immo/ImmoResultsTableModel.groovy
+++ src/main/groovy/agregator/immo/ImmoResultsTableModel.groovy
@@ -10,15 +10,17 @@ import agregator.core.Result;
 
 public class ImmoResultsTableModel extends AbstractTableModel {
 	
-	ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle");
+	ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle")
 	def results = []
 	
 	def columnsName = []
 	
 	public ImmoResultsTableModel() {
-		columnsName << messages.getString("column.title")
+        columnsName << messages.getString("column.site")
+        columnsName << messages.getString("column.title")
+        columnsName << messages.getString("column.description")
+        columnsName << messages.getString("column.price")
 		columnsName << messages.getString("column.url")
-		columnsName << messages.getString("column.site")
 	}
 	
 	public void addResult(Result result){
@@ -27,34 +29,38 @@ public class ImmoResultsTableModel extends AbstractTableModel {
 	}
 	
 	public void clear(){
-		results.removeAll(results);
-		fireTableDataChanged();
+		results.removeAll(results)
+		fireTableDataChanged()
 	}
 	
 	public Class<?> getColumnClass(int columnIndex) {
-		return getValueAt(0, columnIndex).getClass();
+		return getValueAt(0, columnIndex).getClass()
 	}
 	
 	public int getColumnCount() {
-		return columnsName.size;
+		return columnsName.size
 	}
 	
 	public String getColumnName(int columnIndex) {
-		return columnsName[columnIndex];
+		return columnsName[columnIndex]
 	}
 	
 	public int getRowCount() {
-		return results.size;
+		return results.size
 	}
 	
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		switch (columnIndex) {
 			case 0:
-				return results.get(rowIndex).getTitle();
+                return results.get(rowIndex).cartridge.name
 			case 1:
-				return results.get(rowIndex).getUrl();
+                return results.get(rowIndex).title
 			case 2:
-				return results.get(rowIndex).getCartridge().getName();
+				return results.get(rowIndex).description;
+            case 3:
+                return results.get(rowIndex).price;
+            case 4:
+                  return results.get(rowIndex).url;
 			default:
 				return "Error"
 		}
