b0c8c3ff93b120972bdbf19d19ef01700f1faaac
Remi VANKEISBELCK <remi@rvkb.com>
2010-12-03 17:50:09 +0100

    rebranched all cartridges

--- src/main/groovy/agregator/immo/ImmoAgregator.groovy
+++ src/main/groovy/agregator/immo/ImmoAgregator.groovy
@@ -14,12 +14,12 @@ import agregator.immo.cartridges.LogicImmoCartridge
 public class ImmoAgregator extends Agregator {
 
   protected void createCartridges(List cartridges) {
-//    cartridges << new ParuVenduCartridge(this)
-//    cartridges << new SeLogerCartridge(this)
-//    cartridges << new FnaimCartridge(this)
-///    cartridges << new LeboncoinCartridge(this)
-//    cartridges << new LogicImmoCartridge(this)
-    cartridges << new MockImmoCartridgeForUITests("www.foobar.com", this)
+    cartridges << new ParuVenduCartridge(this)
+    cartridges << new SeLogerCartridge(this)
+    cartridges << new FnaimCartridge(this)
+    cartridges << new LeboncoinCartridge(this)
+    cartridges << new LogicImmoCartridge(this)
+//    cartridges << new MockImmoCartridgeForUITests("www.foobar.com", this)
 //    cartridges << new MockImmoCartridgeForUITests("www.yeah.fr", this)
 //    cartridges << new MockImmoCartridgeForUITests("www.shootme.com", this)
   }
